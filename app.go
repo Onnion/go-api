676734b39cf9ae15e02aec6e6cf68215ae3a6a2f
Marcio Henrique <marcio.lima@mangue3.com>
2018-05-15 16:25:08 -0300

    [master] | Teste

---
+++ app.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "asdasd")
+}
+
+func main() {
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/movies", home).Methods("GET")
+	r.HandleFunc("/movies", home).Methods("POST")
+	r.HandleFunc("/movies", home).Methods("PUT")
+	r.HandleFunc("/movies", home).Methods("DELETE")
+
+	// the regex validation inside {id} expecifc what route will access
+	r.HandleFunc("/movies/{id:[0-9]+}", home).Methods("GET")
+	r.HandleFunc("/movies/{id:[a-z]+}", home).Methods("GET")
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+
+}
